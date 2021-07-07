549adf6a5d6947686012ca6d4b2e05dbdc06af89
Olivialamyc <lamemily0209@gmail.com>
2021-07-07 09:10:52 +0800

    updated

---
+++ codecademy_exercise/JS-Dog_Years/app.js
@@ -0,0 +1,10 @@
+const myAge = '23'; //age//
+let earlyYears = '2'; //year//
+earlyYears *= 10.5;
+let laterYears = '2012' ; //lateryear//
+laterYears*= 4; //multiplication//
+console.log(earlyYears);
+console.log(laterYears);
+myAgeInDogYears = earlyYears + laterYears //myAgeInDogYears//
+let mmyName = 'Lam'.toLowerCase(); //myName//
+console.log(`My name is ${mmyName}. I am ${myAgeInDogYears} years old in human years which is DOG AGE years old in dog years.`)
---
+++ codecademy_exercise/JS-Kelvin_Weather/app.js
@@ -0,0 +1,7 @@
+const kelvin = '0';//Kelvin degrees//
+const celsius = kelvin - 273;//Celsius degrees//
+let fahrenheit = celsius * (9/5) + 32;//fahrenheit//
+fahrenheit = Math.floor(fahrenheit);//fahrenheit//
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
+console.log(kelvin)
+
